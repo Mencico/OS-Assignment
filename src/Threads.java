8746845e66ad2b1ba329ed80e93bdeffa028dde2
Min Zhang <zhang.min8@northeastern.edu>
2023-04-27 01:35:52 -0700

    ..

--- src/Threads.java
+++ src/Threads.java
@@ -1,24 +1,32 @@
 public class Threads {
-        public static void main(String[] args) {
-            Thread t1 = new Thread(new ThreadRun(), "The Thread 1");
-            Thread t2 = new Thread(new ThreadRun(), "The Thread 2");
-            Thread t3 = new Thread(new ThreadRun(), "The Thread 3");
-            Thread t4 = new Thread(new ThreadRun(), "The Thread 4");
+    public static void main(String[] args) {
+        Thread th1 = new Thread(new ThreadFunction());
+        th1.setName("Thread 1");
+        Thread th2 = new Thread(new ThreadFunction());
+        th2.setName("Thread 2");
+        Thread th3 = new Thread(new ThreadFunction());
+        th3.setName("Thread 3");
+        Thread th4 = new Thread(new ThreadFunction());
+        th4.setName("Thread 4");
 
-
-
-            t1.start();
-            t2.start();
-            t3.start();
-            t4.start();
-        }
+        th1.start();
+        th2.start();
+        th3.start();
+        th4.start();
+    }
 }
 
-            class ThreadRun implements Runnable {
-                public void run() {
-
-                    System.out.println(Thread.currentThread().getName() + " is running perfectly!");
-                }
+class ThreadFunction implements Runnable {
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("I'm inside parallel threads. Loop: " + Integer.toString(i));
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
+        }
+    }
+}
 
 
